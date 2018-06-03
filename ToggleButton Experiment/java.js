128255d017eb4c8e4f5d4a15993260b8ac8c35b0
Nobody-Inhere <30371652+Nobody-Inhere@users.noreply.github.com>
2018-06-03 11:18:09 -0400

    Update
    Updated repository

---
+++ ToggleButton Experiment/java.js
@@ -0,0 +1,9 @@
+<script>
+var edit = document.getElementById("edit");
+var save = document.getElementById("save");
+
+edit.onclick = function() {
+    save.style.visibility = "visible";
+
+}
+</script>
